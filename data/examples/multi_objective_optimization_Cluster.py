00fb6592028b3820b27b4519aafdd9f6f021b217
SPA75 <113014662+SPA75@users.noreply.github.com>
2024-10-31 13:52:27 +0100

    Update from work

--- data/examples/multi_objective_optimization_Cluster.py
+++ data/examples/multi_objective_optimization_Cluster.py
@@ -56,7 +56,7 @@ def calcul_steps(min_env, max_env, number_of_steps):
 if __name__ == '__main__':
 
     # initialize parameters
-    numberOfOptimizations = 7 # ODD NUMBER number of optimizations in multi objective optimization pareto front
+    numberOfOptimizations = 2 # ODD NUMBER number of optimizations in multi objective optimization pareto front
     if numberOfOptimizations%2==0:
         numberOfOptimizations=numberOfOptimizations+1
     numberOfBuildings = 10
@@ -86,7 +86,7 @@ if __name__ == '__main__':
             inputFilePath = curDir / ".." / "excels" / "IamLenz"
             
             # Input OF CLUSTERING STUDY: inputfileName = "scenario_IamLenz_10_075_TES_GSHP_PV_ST_PVT_mergeON.xls"
-            inputfileName="scenario_IamLenz_TES_allHP_mrgOFF_CostsOK.xls"
+            inputfileName="scenario_IamLenz_TES_ASHP_mrgOFF_CostsOK.xls"
             # inputfileName = "scenario_IamLenz_2_costs_075_TES_GSHP_PV_ST_PVT-CAD.xls"
         
             resultFilePath = r"..\results"
@@ -140,7 +140,7 @@ if __name__ == '__main__':
                              save_file=True,
                              load_file=False,
                              set_scenario=True,
-                             single_scenario=True,                             
+                             single_scenario=False,                             
                              cl_hh=False)
             # create electricity profile based on Romande Energie tarif
             # or spot profile in electricity_spot.csv
@@ -296,7 +296,7 @@ if __name__ == '__main__':
                 network.printEnvImpacts()
         
                 # save results
-                resultFileName = f"results_pareto_mrgOFF_IamLenz_10_allHP_cluster{clN}_hh_{cl_hh}" + str(numberOfBuildings) + '_' + str(opt) + '.xlsx'    # result filename for each optimization
+                resultFileName = f"results_pareto_mrgOFF_IamLenz_10_ASHP_cluster{clN}_hh_{cl_hh}" + str(numberOfBuildings) + '_' + str(opt) + '.xlsx'    # result filename for each optimization
         
                 if not os.path.exists(resultFilePath):
                     os.makedirs(resultFilePath)
@@ -343,7 +343,7 @@ if __name__ == '__main__':
             if not os.path.exists(figureFilePath):
                 os.makedirs(figureFilePath)
         
-            figureFileName = f"Pareto_IamLenz_10_allHP_PV_PVT_ST_075TES_mrgOFF_Cl{clN}_hh_{cl_hh}.png"
+            figureFileName = f"Pareto_IamLenz_10_ASHP_PV_PVT_ST_075TES_mrgOFF_Cl{clN}_hh_{cl_hh}.png"
         
             plotParetoFront(os.path.join(figureFilePath, figureFileName), costsList, envList)
         
